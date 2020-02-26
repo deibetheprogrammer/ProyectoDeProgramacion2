d6348fb15839cf477802f1dbb350ad7e7e6a13fc
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-02-26 09:18:54 -0600

    membresias añadidas a ofertador

--- ProyectoDeProgra2/src/proyectodeprogra2/Personas/Ofertador.java
+++ ProyectoDeProgra2/src/proyectodeprogra2/Personas/Ofertador.java
@@ -13,6 +13,11 @@ import java.util.Date;
  * @author daba5
  */
 public class Ofertador extends PersonaNatural implements Serializable {
+    
+    private boolean membresia_VentaDirecta;
+    private boolean membresia_VentaSubasta;
+    private boolean membresia_Renta;
+    private boolean membresia_Carwash;
 
     public Ofertador() {
     }
@@ -26,4 +31,37 @@ public class Ofertador extends PersonaNatural implements Serializable {
         return String.format("Nickname: %s%nPassword: %s%nPais: %s%nFecha de nacimiento: %s%nCorreo: %s%nNombre: %s%n",nickname,password,pais,birthday,correo,nombre);
     }
 
+    public boolean isMembresia_VentaDirecta() {
+        return membresia_VentaDirecta;
+    }
+
+    public void setMembresia_VentaDirecta(boolean membresia_VentaDirecta) {
+        this.membresia_VentaDirecta = membresia_VentaDirecta;
+    }
+
+    public boolean isMembresia_VentaSubasta() {
+        return membresia_VentaSubasta;
+    }
+
+    public void setMembresia_VentaSubasta(boolean membresia_VentaSubasta) {
+        this.membresia_VentaSubasta = membresia_VentaSubasta;
+    }
+
+    public boolean isMembresia_Renta() {
+        return membresia_Renta;
+    }
+
+    public void setMembresia_Renta(boolean membresia_Renta) {
+        this.membresia_Renta = membresia_Renta;
+    }
+
+    public boolean isMembresia_Carwash() {
+        return membresia_Carwash;
+    }
+
+    public void setMembresia_Carwash(boolean membresia_Carwash) {
+        this.membresia_Carwash = membresia_Carwash;
+    }
+    
+    
 }
